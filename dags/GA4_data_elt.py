3c8c67f7c40a38873842e6c552e53ab3c87bf9d5
robert <robert@robertui-MacBookPro.local>
2025-07-04 17:49:17 +0900

    update elt for ga data

--- dags/GA4_data_elt.py
+++ dags/GA4_data_elt.py
@@ -4,13 +4,7 @@ from datetime import datetime
 from airflow.decorators import task
 from airflow.models import Variable
 from util.read_sql_file import read_sql_file
-import os 
-
-ENV = os.getenv('ENV', 'dev')
-
-default_args = {
-    'start_date': datetime(2024, 12, 31) if ENV == 'prod' else datetime(2025, 6, 5),
-}
+from util.bigquery_custom_sensor import BigQueryDataSensor
 
 @task 
 def start_task(**context):
@@ -22,18 +16,18 @@ def end_task():
     print("end")
 
 with DAG(   
-    dag_id= f'ga4_raw_data_elt_v1.0_{ENV}',
-    default_args=default_args,
-    schedule= '10 8 * * *' if ENV == 'prod' else '10 2 * * *',  
+    dag_id= f'ga4_raw_data_elt_v1.0_prod',
+    start_date=datetime(2025, 6, 29),
+    schedule= '10 8 * * *',  
     catchup=True,
-    tags=['ga4', 'raw_data_elt', 'prod'] if ENV == 'prod' else ['ga4', 'raw_data_elt', 'dev'],
+    tags=['ga4', 'raw_data_elt', 'prod'] 
 ) as dag:
     start = start_task()
     end = end_task()
 
     create_dataset = BigQueryCreateEmptyDatasetOperator(
         task_id='create_ga4_events_dataset',
-        dataset_id=Variable.get('GA4_PROD_RAW_DATASET_ID') if ENV == 'prod' else Variable.get('GA4_DEV_RAW_DATASET_ID'),
+        dataset_id=Variable.get('GA4_PROD_RAW_DATASET_ID'),
         project_id=Variable.get('BIG_QUERY_PROJECT_ID'),
         location='US',
         exists_ok=True,
@@ -50,13 +44,27 @@ with DAG(
                     'sql/GA4_elt',
                     ds='{{ ds }}',
                     project_id=Variable.get('BIG_QUERY_PROJECT_ID'),
-                    source_dataset_id=Variable.get('GA4_PROD_DATASET_ID') if ENV == 'prod' else Variable.get('GA4_DEV_DATASET_ID'),
-                    target_dataset_id=Variable.get('GA4_PROD_RAW_DATASET_ID') if ENV == 'prod' else Variable.get('GA4_DEV_RAW_DATASET_ID'),
+                    source_dataset_id=Variable.get('GA4_PROD_DATASET_ID'),
+                    target_dataset_id=Variable.get('GA4_PROD_RAW_DATASET_ID'),
                     target_table_id='raw_data'
                 ),
                 "useLegacySql": False,
             }
         },
+    )
+
+    check_table_exists = BigQueryDataSensor(
+        task_id='check_table_exists',
+        sql_file='check_ga4_data_imported.sql',
+        sql_path='sql/GA4_elt',
+        project_id=Variable.get('BIG_QUERY_PROJECT_ID'),
+        source_dataset_id=Variable.get('GA4_PROD_DATASET_ID'),
+        source_table_id=None,
+        event_name=None,
+        gcp_conn_id='google_cloud_default',
+        poke_interval=int(Variable.get('CHECK_INTERVAL')),
+        timeout=int(Variable.get('CHECK_TIMEOUT')),
+        mode='poke'
     )
 
     run_partition_insert = BigQueryInsertJobOperator(
@@ -69,8 +77,8 @@ with DAG(
                     'sql/GA4_elt',
                     ds='{{ ds }}',      
                     project_id=Variable.get('BIG_QUERY_PROJECT_ID'),
-                    source_dataset_id=Variable.get('GA4_PROD_DATASET_ID') if ENV == 'prod' else Variable.get('GA4_DEV_DATASET_ID'),
-                    target_dataset_id=Variable.get('GA4_PROD_RAW_DATASET_ID') if ENV == 'prod' else Variable.get('GA4_DEV_RAW_DATASET_ID'),
+                    source_dataset_id=Variable.get('GA4_PROD_DATASET_ID'),
+                    target_dataset_id=Variable.get('GA4_PROD_RAW_DATASET_ID'),
                     target_table_id='raw_data'
                 ),
                 "useLegacySql": False,
@@ -78,4 +86,4 @@ with DAG(
         },
     )
 
-    start >> create_dataset >> create_table >> run_partition_insert >> end
+    start >> create_dataset >> create_table >> check_table_exists >> run_partition_insert >> end
--- dags/GA4_event_elt.py
+++ dags/GA4_event_elt.py
@@ -2,15 +2,11 @@ from airflow import DAG
 from airflow.operators.dummy import DummyOperator
 from datetime import datetime
 from airflow.decorators import task
-import os
 from util.read_sql_file import read_sql_file
 from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
 from airflow.models import Variable
 from airflow.utils.task_group import TaskGroup
 from util.bigquery_custom_sensor import BigQueryDataSensor
-
-ENV = os.getenv('ENV', 'dev')
-
 
 @task 
 def start_task(**context):
@@ -24,24 +20,68 @@ def end_task():
     print("end")
 
 with DAG(
-    dag_id=f'ga4_event_elt_{ENV}',
-    start_date=datetime(2024, 12, 31) if ENV == 'prod' else datetime(2025, 6, 5),
-    schedule='15 8 * * *' if ENV == 'prod' else '15 2 * * *',
+    dag_id=f'ga4_event_elt_prod',
+    start_date=datetime(2025, 6, 29),
+    schedule='15 8 * * *',
     catchup=True,
-    tags=['ga4', 'event_elt', ENV],
+    tags=['ga4', 'event_elt', 'prod'],
 ) as dag:
 
     start = start_task()
+
+    with TaskGroup(group_id='user_pseudo_id_elt') as user_pseudo_id_elt:
+        create_user_pseudo_id_table = BigQueryInsertJobOperator(
+            task_id='create_user_pseudo_id_table',
+            gcp_conn_id='google_cloud_default',
+            configuration={
+                "query": {
+                    "query": read_sql_file(
+                        'create_user_pseudo_id_table.sql', 
+                        'sql/GA4_elt',
+                        ds='{{ ds }}',
+                        project_id=Variable.get('BIG_QUERY_PROJECT_ID'),
+                        source_dataset_id=Variable.get('GA4_PROD_RAW_DATASET_ID'),
+                        target_dataset_id=Variable.get('GA4_PROD_RAW_DATASET_ID'),
+                        target_table_id='user_pseudo_id'
+                    ),
+                    "useLegacySql": False,
+                }
+            },
+        )
+
+        insert_user_pseudo_id_table = BigQueryInsertJobOperator(
+            task_id='insert_user_pseudo_id_table',
+            gcp_conn_id='google_cloud_default',
+            configuration={
+                "query": {
+                    "query": read_sql_file(
+                        'insert_user_pseudo_id_table.sql', 
+                        'sql/GA4_elt',
+                        ds='{{ ds }}',
+                        project_id=Variable.get('BIG_QUERY_PROJECT_ID'),
+                        source_dataset_id=Variable.get('GA4_PROD_RAW_DATASET_ID'),
+                        target_dataset_id=Variable.get('GA4_PROD_RAW_DATASET_ID'),
+                        target_table_id='user_pseudo_id'
+                    ),
+                    "useLegacySql": False,
+                }
+            },
+        )
+
+        create_user_pseudo_id_table >> insert_user_pseudo_id_table
 
     with TaskGroup(group_id='view_item_event_elt') as view_item_event_elt:
         check_view_item_raw_data_exists = BigQueryDataSensor(
             task_id='check_view_item_raw_data_exists',
             sql_file='event_table_check.sql',
             sql_path='sql/GA4_elt',
+            project_id=Variable.get('BIG_QUERY_PROJECT_ID'),
+            source_dataset_id=Variable.get('GA4_PROD_RAW_DATASET_ID'),
+            source_table_id='raw_data',
             event_name='view_item',
             gcp_conn_id='google_cloud_default',
-            poke_interval=60,
-            timeout=300,
+            poke_interval=int(Variable.get('CHECK_INTERVAL')),
+            timeout=int(Variable.get('CHECK_TIMEOUT')),
             mode='poke'
         )
 
@@ -55,8 +95,8 @@ with DAG(
                         'sql/GA4_elt',
                         ds='{{ ds }}',
                         project_id=Variable.get('BIG_QUERY_PROJECT_ID'),
-                        source_dataset_id=Variable.get('GA4_PROD_RAW_DATASET_ID') if ENV == 'prod' else Variable.get('GA4_DEV_RAW_DATASET_ID'),
-                        target_dataset_id=Variable.get('GA4_PROD_RAW_DATASET_ID') if ENV == 'prod' else Variable.get('GA4_DEV_RAW_DATASET_ID'),
+                        source_dataset_id=Variable.get('GA4_PROD_RAW_DATASET_ID'),
+                        target_dataset_id=Variable.get('GA4_PROD_RAW_DATASET_ID'),
                         target_table_id='view_item_event'
                     ),
                     "useLegacySql": False,
@@ -74,8 +114,8 @@ with DAG(
                         'sql/GA4_elt',
                         ds='{{ ds }}',
                         project_id=Variable.get('BIG_QUERY_PROJECT_ID'),
-                        source_dataset_id=Variable.get('GA4_PROD_RAW_DATASET_ID') if ENV == 'prod' else Variable.get('GA4_DEV_RAW_DATASET_ID'),
-                        target_dataset_id=Variable.get('GA4_PROD_RAW_DATASET_ID') if ENV == 'prod' else Variable.get('GA4_DEV_RAW_DATASET_ID'),
+                        source_dataset_id=Variable.get('GA4_PROD_RAW_DATASET_ID'),
+                        target_dataset_id=Variable.get('GA4_PROD_RAW_DATASET_ID'),
                         target_table_id='view_item_event'
                     ),  
                     "useLegacySql": False,
@@ -84,7 +124,119 @@ with DAG(
         )
 
         check_view_item_raw_data_exists >> create_view_item_event_table >> insert_view_item_event_table
+
+    with TaskGroup(group_id='add_to_cart_event_elt') as add_to_cart_event_elt:
+        check_add_to_cart_raw_data_exists = BigQueryDataSensor(
+            task_id='check_add_to_cart_raw_data_exists',
+            sql_file='event_table_check.sql',
+            sql_path='sql/GA4_elt',
+            project_id=Variable.get('BIG_QUERY_PROJECT_ID'),
+            source_dataset_id=Variable.get('GA4_PROD_RAW_DATASET_ID'),
+            source_table_id='raw_data',
+            event_name='add_to_cart',
+            gcp_conn_id='google_cloud_default',
+            poke_interval=int(Variable.get('CHECK_INTERVAL')),
+            timeout=int(Variable.get('CHECK_TIMEOUT')),
+            mode='poke'
+        )
+
+        create_add_to_cart_event_table = BigQueryInsertJobOperator(
+            task_id='create_add_to_cart_event_table',
+            gcp_conn_id='google_cloud_default',
+            configuration={
+                "query": {
+                    "query": read_sql_file(
+                        'create_ga4_add_to_cart_event_table.sql', 
+                        'sql/GA4_elt',
+                        ds='{{ ds }}',
+                        project_id=Variable.get('BIG_QUERY_PROJECT_ID'),
+                        source_dataset_id=Variable.get('GA4_PROD_RAW_DATASET_ID'),
+                        target_dataset_id=Variable.get('GA4_PROD_RAW_DATASET_ID'),
+                        target_table_id='add_to_cart_event'
+                    ),
+                    "useLegacySql": False,
+                }
+            },
+        )
+
+        insert_add_to_cart_event_table = BigQueryInsertJobOperator(
+            task_id='insert_add_to_cart_event_table',
+            gcp_conn_id='google_cloud_default',
+            configuration={
+                "query": {
+                    "query": read_sql_file(
+                        'insert_add_to_cart_event_table.sql', 
+                        'sql/GA4_elt',
+                        ds='{{ ds }}',
+                        project_id=Variable.get('BIG_QUERY_PROJECT_ID'),
+                        source_dataset_id=Variable.get('GA4_PROD_RAW_DATASET_ID'),
+                        target_dataset_id=Variable.get('GA4_PROD_RAW_DATASET_ID'),
+                        target_table_id='add_to_cart_event'
+                    ),  
+                    "useLegacySql": False,
+                }
+            },
+        )
+
+        check_add_to_cart_raw_data_exists >> create_add_to_cart_event_table >> insert_add_to_cart_event_table
         
+    with TaskGroup(group_id='add_to_wishlist_event_elt') as add_to_wishlist_event_elt:
+        check_add_to_wishlist_raw_data_exists = BigQueryDataSensor(
+            task_id='check_add_to_wishlist_raw_data_exists',
+            sql_file='event_table_check.sql',
+            sql_path='sql/GA4_elt',
+            project_id=Variable.get('BIG_QUERY_PROJECT_ID'),
+            source_dataset_id=Variable.get('GA4_PROD_RAW_DATASET_ID'),
+            source_table_id='raw_data',
+            event_name='add_to_wishlist',
+            gcp_conn_id='google_cloud_default',
+            poke_interval=int(Variable.get('CHECK_INTERVAL')),
+            timeout=int(Variable.get('CHECK_TIMEOUT')),
+            mode='poke'
+        )
+
+        create_add_to_wishlist_event_table = BigQueryInsertJobOperator(
+            task_id='create_add_to_wishlist_event_table',
+            gcp_conn_id='google_cloud_default',
+            configuration={
+                "query": {
+                    "query": read_sql_file(
+                        'create_ga4_add_to_wishlist_event_table.sql', 
+                        'sql/GA4_elt',
+                        ds='{{ ds }}',
+                        project_id=Variable.get('BIG_QUERY_PROJECT_ID'),
+                        source_dataset_id=Variable.get('GA4_PROD_RAW_DATASET_ID'),
+                        target_dataset_id=Variable.get('GA4_PROD_RAW_DATASET_ID'),
+                        target_table_id='add_to_wishlist_event'
+                    ),
+                    "useLegacySql": False,
+                }
+            },
+        )
+
+        insert_add_to_wishlist_event_table = BigQueryInsertJobOperator(
+            task_id='insert_add_to_wishlist_event_table',
+            gcp_conn_id='google_cloud_default',
+            configuration={
+                "query": {
+                    "query": read_sql_file(
+                        'insert_add_to_wishlist_event_table.sql', 
+                        'sql/GA4_elt',
+                        ds='{{ ds }}',
+                        project_id=Variable.get('BIG_QUERY_PROJECT_ID'),
+                        source_dataset_id=Variable.get('GA4_PROD_RAW_DATASET_ID'),
+                        target_dataset_id=Variable.get('GA4_PROD_RAW_DATASET_ID'),
+                        target_table_id='add_to_wishlist_event'
+                    ),
+                    "useLegacySql": False,
+                }
+            },
+        )
+
+        check_add_to_wishlist_raw_data_exists >> create_add_to_wishlist_event_table >> insert_add_to_wishlist_event_table
+
+    event_elt = [ view_item_event_elt, add_to_cart_event_elt, add_to_wishlist_event_elt]
+    
     end = end_task()
 
-    start >> view_item_event_elt >> end
+    start >> user_pseudo_id_elt >> event_elt >> end
